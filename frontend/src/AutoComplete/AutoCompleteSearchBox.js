b4578d895bc6853a9209a30ac886cff3d98f565d
Ali <a.k.ezzeddine@gmail.com>
2023-07-10 14:22:57 -0400

    fixed autocomplete not closing when it should be and not allowing results after backspace is hit

--- frontend/src/AutoComplete/AutoCompleteSearchBox.js
+++ frontend/src/AutoComplete/AutoCompleteSearchBox.js
@@ -1,14 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import data from "../BookData/bookData.json"
 import "./AutoCompleteSearchBox.css"
 
 const AutocompleteSearchBox = ({ setInputValue, inputValue, setIsSelected, isSelected }) => {
     const [filteredOptions, setFilteredOptions] = useState([]);
+    const componentRef = useRef(null);
     const filterOptions = (input) => {
         const filtered = data.Books.filter(option =>
             option.title.toLowerCase().includes(input.toLowerCase())
         );
         setFilteredOptions(filtered);
+    };
+    const handleKeyDown = (e) => {
+        if (e.keyCode === 8) {
+            // Backspace key was pressed
+            setIsSelected(false)
+        }
     };
     const handleClick = (title) => {
         setInputValue(title)
@@ -16,12 +23,27 @@ const AutocompleteSearchBox = ({ setInputValue, inputValue, setIsSelected, isSel
         setIsSelected(true)
     }
     useEffect(() => {
-        if (inputValue.length >= 1) {
+        if (inputValue.length) {
             filterOptions(inputValue);
         } else {
             setFilteredOptions([])
         }
     }, [inputValue, setInputValue, isSelected, setFilteredOptions])
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (componentRef.current && !componentRef.current.contains(event.target)) {
+                // Click occurred outside the component
+                // Perform the action to close the component here
+                setFilteredOptions([])
+            }
+        };
+
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, []);
     return (
         <div style={{ width: "650px" }}>
             <input
@@ -29,9 +51,20 @@ const AutocompleteSearchBox = ({ setInputValue, inputValue, setIsSelected, isSel
                 value={inputValue}
                 placeholder='Guess a book'
                 onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={handleKeyDown}
+                ref={componentRef}
             />
-            {filteredOptions.length > 1 && (
-                <div className='filter-options'>
+            {filteredOptions.length > 4 && !isSelected && (
+                <div className='filter-options-scroll'>
+                    {filteredOptions.map(option => (
+                        <span className='book-title-span' key={option.title} onClick={(e) => handleClick(option.title)}>
+                            {option.title}
+                        </span>
+                    ))}
+                </div>
+            )}
+            {filteredOptions.length <= 4 && filteredOptions.length >= 1 && !isSelected && (
+                <div className='filter-options-noscroll'>
                     {filteredOptions.map(option => (
                         <span className='book-title-span' key={option.title} onClick={(e) => handleClick(option.title)}>
                             {option.title}
